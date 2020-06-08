0bc07cbe0b8a9168b44f7ed4c3bfaf44441878ec
dferdinand21 <dferdinand21business@gmail.com>
2020-06-08 18:20:01 +0700

    Small update on minimap, added the quit function on the menu

---
+++ OfficialInsaneProject/Assets/Script/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    // Update is called once per frame
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}

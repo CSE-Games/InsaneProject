0750d5f6375882617796b7154b6429fd3d672f24
dferdinand21 <dferdinand21business@gmail.com>
2020-06-09 17:49:16 +0700

    added volume and brightness settings on options menu

---
+++ OfficialInsaneProject/Assets/Script/LightValueChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightValueChange : MonoBehaviour
+{
+
+    private Light2D light;
+
+    private float intensity = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = intensity;
+    }
+
+    public void SetIntensity(float inte)
+    {
+        intensity = inte;
+    }
+}
